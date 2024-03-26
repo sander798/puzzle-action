ce694f380dca7c8bd25c206363853237eac88129
Alexander Evans <sander7989@gmail.com>
2024-03-26 11:26:52 -0600

    Refactor player entity to be defined as static class in Slime superclass

--- core/src/main/java/com/game/Slime.java
+++ core/src/main/java/com/game/Slime.java
@@ -217,4 +217,17 @@ public abstract class Slime extends Entity {
     public void onCollision(PlayScene play, Entity collidingEntity) {
 
     }
+
+    public static class SlimeGreen extends Slime {
+        public SlimeGreen(float x, float y) {
+            super("ply5",
+                new TextureAnimation[]{
+                    new TextureAnimation(Load.getAnimations()[0].getTextureRegion(), 16, 4, 0.5f),
+                    new TextureAnimation(Load.getAnimations()[1].getTextureRegion(), 16, 4, 0.1f),
+                    new TextureAnimation(Load.getAnimations()[2].getTextureRegion(), 16, 4, 0.1f),
+                    new TextureAnimation(Load.getAnimations()[3].getTextureRegion(), 16, 4, 0.1f),
+                    new TextureAnimation(Load.getAnimations()[4].getTextureRegion(), 16, 4, 0.1f),
+                }, x * Game.BASE_TILE_SIZE, y * Game.BASE_TILE_SIZE);
+        }
+    }
 }
